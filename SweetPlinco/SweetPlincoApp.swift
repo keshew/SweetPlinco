235a0eac953ed3f75235f032b556097295e23013
keshew <artyom.korotckov2017@yandex.ru>
2024-11-24 18:06:24 +0500

    Initial Commit

---
+++ SweetPlinco/SweetPlincoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetPlincoApp.swift
+//  SweetPlinco
+//
+//  Created by Артём Коротков on 24.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SweetPlincoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
